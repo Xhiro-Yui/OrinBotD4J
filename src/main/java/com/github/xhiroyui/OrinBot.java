f5070eaf597fc43a9c1eccd7c72210324017056d
Xhiro Yui <xhiro.yui@gmail.com>
2018-01-25 11:49:43 +0800

    Possible heroku deployment?

--- src/main/java/com/github/xhiroyui/OrinBot.java
+++ src/main/java/com/github/xhiroyui/OrinBot.java
@@ -5,7 +5,15 @@ import sx.blah.discord.api.IDiscordClient;
 public class OrinBot {
 	
 	public static void main(String[] args) {
-		IDiscordClient client = new DiscordClientBuilder().buildClient(args[0]);
+		IDiscordClient client;
+		try {
+//			For heroku deployment? Not sure if it works
+			 client = new DiscordClientBuilder().buildClient(System.getenv("TOKEN"));
+		}
+		catch (Exception e) {
+			client = new DiscordClientBuilder().buildClient(args[0]);
+		}
+		
 		ModuleLoader modLoader = new ModuleLoader(client);
 		
 		modLoader.loadMandatoryModules();
